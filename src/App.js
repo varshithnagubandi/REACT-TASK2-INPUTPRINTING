d143d7fb46f6c251c4750083cc9bf1cfb0c459b8
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-07-23 01:49:41 +0530

    inputprinting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  function Inputhandle(event) {
+    setName(event.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={name} type="text" onChange={Inputhandle} />
+      <h1>Enter name : {name}</h1>
     </div>
   );
 }
